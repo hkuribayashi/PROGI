39c05d6797a692e12172ec9c44c208a81f0ad2b4
hugokuribayashi <hkuribayashi@gmail.com>
2025-06-26 16:12:15 -0300

    Exemplos de 2025-05-29

--- src/Exemplo51.java
+++ src/Exemplo51.java
@@ -15,10 +15,20 @@ public class Exemplo51 {
     }
     
     public static boolean verificarPalidromo(char[] meuarray) {
-        return  false;
+        int inicio = 0;
+        int fim = meuarray.length - 1;
+        while (inicio <= fim){
+            if (meuarray[inicio] != meuarray[fim])
+                return false;
+            inicio++;
+            fim--;
+        }
+        return  true;
     }
 
     public static void main(String[] args) {
-
+        char[] meuVetor = criarArray();
+        boolean verificar = verificarPalidromo(meuVetor);
+        System.out.println("Resultado: " + verificar);
     }
 }
---
+++ src/Exemplo52.java
@@ -0,0 +1,28 @@
+public class Exemplo52 {
+
+    public static double[] criarArray() {
+        double[] vetor = {7, 4, 9, 10};
+        return vetor;
+    }
+
+    public static double[] elevarQuadrado(double[] meuArray) {
+        double[] novoArray = new double[meuArray.length];
+        for (int i=0; i<meuArray.length; i++) {
+            novoArray[i] = meuArray[i] * meuArray[i];
+        }
+        return novoArray;
+    }
+
+    public static void imprimirArray(double[] vetor) {
+        for (int i=0; i<vetor.length; i++)
+            System.out.print(vetor[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        double[] meuVetor = criarArray();
+        double[] novoVetor = elevarQuadrado(meuVetor);
+        imprimirArray(meuVetor);
+        System.out.println();
+        imprimirArray(novoVetor);
+    }
+}
